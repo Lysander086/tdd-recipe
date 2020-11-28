eb0a36bb47ad2651f06c84a3dac07541b1c88a67
z <z@example.com>
2020-11-28 21:03:03 +0800

    c6 unit test

---
+++ README.md
@@ -0,0 +1,1 @@
+original project: [recipe-app-api](https://github.com/LondonAppDeveloper/recipe-app-api)
--- app/app/settings.py
+++ app/app/settings.py
@@ -14,7 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "app.settings")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
