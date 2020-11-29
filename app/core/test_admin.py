644111d4bc69710ed7c54a6f8b100d192d3af64b
z <z@example.com>
2020-11-29 21:52:15 +0800

    c8 Modify Django admin to list our custom user model

---
+++ app/core/test_admin.py
@@ -0,0 +1,30 @@
+from django.test import TestCase, Client
+from django.contrib.auth import get_user_model
+
+# generate url for django project
+from django.urls import reverse
+from django.test import Client
+
+
+class AdminSiteTests(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+                email='admin@lys.com',
+                password='test1234'
+            )
+        self.client.force_login(self.admin_user)
+        self.user = get_user_model().objects.create_user(
+            email = 'normal_user@lys.com',
+            password='test1234',
+            name='Test user full name'
+        )
+
+    def test_user_listed(self):
+        url = reverse('admin:core_user_changelist')
+        res = self.client.get(url)
+
+        self.assertContains(res, self.admin_user.email)
+        self.assertContains(res, self.user.email)
+        
