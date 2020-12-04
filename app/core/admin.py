92478663263968e1e5a0e75d09228222ca81ad53
z <z@example.com>
2020-12-04 14:18:54 +0800

    success  8. Setup Django admin    3. Modify Django admin to support changing user model

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext as _
+
 
 from core import models
 
@@ -7,6 +9,17 @@ from core import models
 class UserAdmin(BaseUserAdmin):
     ordering = ['id']
     list_display = ['email', 'name']
+
+    fieldsets = (
+        # None: title for the section
+        (None, {'fields': ('email', 'password')}),
+        (_('Personal Info'), {'fields': ('name', )}),  # create personal info
+        (
+            _('Permissions'),
+            {'fields': ('is_active', 'is_staff', 'is_superuser')}
+        ),
+        (_('Important dates'), {'fields':('last_login', )})
+    )
 
 
 admin.site.register(models.User, UserAdmin)
--- app/core/test_admin.py
+++ app/core/test_admin.py
@@ -11,12 +11,12 @@ class AdminSiteTests(TestCase):
     def setUp(self):
         self.client = Client()
         self.admin_user = get_user_model().objects.create_superuser(
-                email='admin@lys.com',
-                password='test1234'
-            )
+            email='admin@lys.com',
+            password='test1234'
+        )
         self.client.force_login(self.admin_user)
         self.user = get_user_model().objects.create_user(
-            email = 'normal_user@lys.com',
+            email='normal_user@lys.com',
             password='test1234',
             name='Test user full name'
         )
@@ -27,4 +27,18 @@ class AdminSiteTests(TestCase):
 
         self.assertContains(res, self.admin_user.email)
         self.assertContains(res, self.user.email)
-        
+
+    def test_user_change_page(self):
+        url = reverse('admin:core_user_change', args=[self.user.id])
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code , 200)
+
+    # def test_create_user_page(self):
+    #     """Test that the create user page works"""
+    #     url = reverse('admin:core_user_add')
+    #     res = self.client.get(url)
+
+    #     self.assertEqual(res.status_code , 200) 
+
+         
