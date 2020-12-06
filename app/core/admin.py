4048dafdb5a2c8356341b7365e6015858241ce1c
z <z@example.com>
2020-12-06 10:30:22 +0800

    8. Setup Django admin    4. Modify Django admin to support creating users

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.utils.translation import gettext as _
-
-
-from core import models
+from . import models
 
 
 class UserAdmin(BaseUserAdmin):
@@ -18,7 +16,14 @@ class UserAdmin(BaseUserAdmin):
             _('Permissions'),
             {'fields': ('is_active', 'is_staff', 'is_superuser')}
         ),
-        (_('Important dates'), {'fields':('last_login', )})
+        (_('Important dates'), {'fields': ('last_login', )})
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2')
+        }),
     )
 
 
